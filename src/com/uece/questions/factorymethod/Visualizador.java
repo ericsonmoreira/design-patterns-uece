ee8300b1ab68c2d59c6e35e42ef9e97a92aa2be1
ericsonmoreira <moreira.ericson@gmail.com>
2021-12-09 18:53:54 -0300

    add question 4 (init)

--- src/com/uece/questions/factorymethod/Visualizador.java
+++ src/com/uece/questions/factorymethod/Visualizador.java
@@ -1,13 +1,13 @@
 package com.uece.questions.factorymethod;
 
 /**
- * Você foi contratado por uma empresa que desenvolve aplicações para  edição e manipulação de imagens. Seu chefe
- * apresentou para você as seguintes classes: (classes exibidas no texto do exercício). Essas classes são responsáveis
- * por carregar imagens do tipo BMP. Seu chefe mandou que  você alterasse o código de forma que a criação de novos
- * visualizadores de imagem ficasse  mais flexível. Crie as seguintes classes: VisualizadorJPG – que visualiza imagens
- * do tipo JPG e  ImagemJPG – que trata de imagens JPG. O código deve ser desenvolvido de forma que a  criação de
- * qualquer outro visualizador seja rápida e flexível. Para resolver esse problema  altere o código acima usando o
- * padrão Factory Method.
+ * 3. Factory Method: Você foi contratado por uma empresa que desenvolve aplicações para  edição e manipulação de
+ * imagens. Seu chefe apresentou para você as seguintes classes: (classes exibidas no texto do exercício). Essas
+ * classes são responsáveis por carregar imagens do tipo BMP. Seu chefe mandou que  você alterasse o código de forma
+ * que a criação de novos visualizadores de imagem ficasse  mais flexível. Crie as seguintes classes:
+ * VisualizadorJPG – que visualiza imagens do tipo JPG e  ImagemJPG – que trata de imagens JPG. O código deve ser
+ * desenvolvido de forma que a  criação de qualquer outro visualizador seja rápida e flexível. Para resolver esse
+ * problema  altere o código acima usando o padrão Factory Method.
  */
 
 public class Visualizador{
